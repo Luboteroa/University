da68aa1f14bf5c4b19901f038529084ce9c88c02
Luis Carlos Botero A <luboteroa@unal.edu.co>
2022-08-12 18:44:16 -0500

    Java curse

--- 2022-II/Programación en Java/Hello World/src/HelloWorld.java
+++ 2022-II/Programación en Java/Hello World/src/HelloWorld.java
@@ -2,9 +2,27 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello World!");
-        System.out.println("I am learning Java.");
-        System.out.println("It is awesome!");
+        //This is a comment
+        long CanNotStoreThisInint = 2147483648L;
+
+        String name = "Luis ";
+        final int numFinal = 9; //Final means that won't change in the future even the programmer wants to
+
+        int num = 1;
+        num++;
+
+        System.out.println(name + num);
+
+        /*ASCII CODING
+        char myVar1 = 65, myVar2 = 60, myVar3 = 62;
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);
+         */
+
+        String txt = "Please locate where 'locate' occurs!";
+        System.out.println(txt.indexOf("locate")); // Outputs 7
+
         Scanner sc = new Scanner(System.in);
         sc.nextLine();
     }
