8f63b49a2897fef8f4da7658d3e9419987b62f69
Luis Carlos Botero A <luboteroa@unal.edu.co>
2022-08-14 18:22:24 -0500

    Some Java Interactions

--- 2022-II/Programación en Java/Hello World/src/HelloWorld.java
+++ 2022-II/Programación en Java/Hello World/src/HelloWorld.java
@@ -20,8 +20,57 @@ public class HelloWorld {
         System.out.println(myVar3);
          */
 
-        String txt = "Please locate where 'locate' occurs!";
-        System.out.println(txt.indexOf("locate")); // Outputs 7
+        /* String txt = "Please locate where locate occurs!";
+        System.out.println(txt.indexOf("locate")); // Outputs 7 */
+
+        /* String firstName = "Luis";
+        String lastName = "Botero";
+        System.out.println("We can do: " + firstName + " " + lastName);
+        System.out.println("Or: " + firstName.concat(lastName)); */
+
+        //We use scape characters for some issues that we can have
+        /* String txt = "We are so-called \"Vikings\" from the north.";
+        String txt2 = " It\'s alright";
+        String txt3 = " The character \\ is called backslash.";
+        System.out.println(txt + txt2 + txt3); */
+
+        //if else if else and switch
+        /* int x = 20;
+        int y = 10;
+        if(x>y)
+        {
+            System.out.println("x is greater than y");
+        } */
+
+        //Short hand if else
+        // variable = (condition) ? expressionTrue : expressionFalse;
+        /* int x = 8;
+        int y = 10;
+        String result = (x>y) ? "x is greater than y" : "y is greater than x";
+        System.out.println(result); */
+
+        //Java for statements
+        /* for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                continue;
+            }
+            System.out.println(i);
+        } */
+
+        //Java Arrays
+        /* String[] cars = {"Volvo", "BMW", "Renault", "Mazda"}; */
+
+        //Multidimensional Arrays
+        /*int[][] myNumbers = { {1,2,3,4}, {5,6,7} };
+        int x = myNumbers[1][2]; //Access the third element (2) in the second array (1)
+        System.out.println(x);*/
+
+        //For inside other for loops
+        /*for (int i = 0; i < myNumbers.length; ++i) {
+            for(int j = 0; j < myNumbers[i].length; ++j) {
+                System.out.println(myNumbers[i][j]);
+            }
+        }*/
 
         Scanner sc = new Scanner(System.in);
         sc.nextLine();
